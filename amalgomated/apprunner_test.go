47ab879d8e18629050965b87342932d63ec5c25e
Nick Miyake <nmiyake@palantir.com>
2016-11-15 14:06:34 -0800

    Initial commit

---
+++ amalgomated/apprunner_test.go
@@ -0,0 +1,101 @@
+// Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package amalgomated_test
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/palantir/amalgomate/amalgomated"
+)
+
+type cmdString string
+
+func (s cmdString) Name() string {
+	return string(s)
+}
+
+func TestRunApp(t *testing.T) {
+	for i, currCase := range []struct {
+		args             []string
+		fset             func() *flag.FlagSet
+		expectedExitCode int
+		expectedOutput   string
+	}{
+		// running non-proxy command executes app function
+		{
+			args: []string{
+				"arg0",
+				"arg1",
+			},
+			expectedExitCode: 13,
+			expectedOutput:   "[arg0 arg1]\n",
+		},
+		// flags are passed to non-proxy command
+		{
+			args: []string{
+				"arg0",
+				"--global-flag",
+				"arg1",
+				"--command-flag",
+				"flag-value",
+			},
+			expectedExitCode: 13,
+			expectedOutput:   "[arg0 --global-flag arg1 --command-flag flag-value]\n",
+		},
+		// running proxy command executes proxy
+		{
+			args: []string{
+				"arg0",
+				amalgomated.ProxyCmdPrefix + "foo",
+			},
+			expectedExitCode: 0,
+			expectedOutput:   "foo\n",
+		},
+		// providing flag set ignores flag values
+		{
+			args: []string{
+				"arg0",
+				"--string-flag",
+				"flag_value",
+				amalgomated.ProxyCmdPrefix + "foo",
+			},
+			fset: func() *flag.FlagSet {
+				fset := flag.NewFlagSet("fset", flag.ContinueOnError)
+				_ = fset.String("string-flag", "", "")
+				return fset
+			},
+			expectedExitCode: 0,
+			expectedOutput:   "foo\n",
+		},
+	} {
+		runAppOutput := &bytes.Buffer{}
+		cmdWithRunner, err := amalgomated.NewCmdWithRunner("foo", func() {
+			fmt.Fprintln(runAppOutput, "foo")
+		})
+		require.NoError(t, err, "Case %d", i)
+
+		cmdSet, err := amalgomated.NewStringCmdSetForRunners(cmdWithRunner)
+		require.NoError(t, err, "Case %d", i)
+		cmdLibrary := amalgomated.NewCmdLibrary(cmdSet)
+		appFunc := func(osArgs []string) int {
+			fmt.Fprintln(runAppOutput, osArgs)
+			return currCase.expectedExitCode
+		}
+
+		var fset *flag.FlagSet
+		if currCase.fset != nil {
+			fset = currCase.fset()
+		}
+		actualExitCode := amalgomated.RunApp(currCase.args, fset, cmdLibrary, appFunc)
+		assert.Equal(t, currCase.expectedOutput, runAppOutput.String(), "Case %d", i)
+		assert.Equal(t, currCase.expectedExitCode, actualExitCode, "Case %d", i)
+	}
+}
---
+++ amalgomated/cmder_test.go
@@ -0,0 +1,45 @@
+// Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package amalgomated_test
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/palantir/amalgomate/amalgomated"
+)
+
+type testCmder exec.Cmd
+
+func (c *testCmder) Cmd(args []string, cmdWd string) *exec.Cmd {
+	c.Args = args
+	c.Dir = cmdWd
+	return (*exec.Cmd)(c)
+}
+
+func (c *testCmder) Run() ([]byte, error) {
+	if len(c.Args) > 0 && c.Args[0] == "error" {
+		return nil, fmt.Errorf("testCmder returned error based on provided arguments: %v", c.Args)
+	}
+	return []byte(fmt.Sprintf("Cmd: %v, args: %v, cmdWd: %v", c.Path, c.Args, c.Dir)), nil
+}
+
+func TestRunnerWithPrependedArgs(t *testing.T) {
+	prepended := []string{"prepended 1", "prepended 2"}
+	c := amalgomated.CmderWithPrependedArgs((*testCmder)(exec.Command("/bin/test")), prepended...)
+
+	provided := []string{"provided 1", "provided 2"}
+	wd := "wd"
+
+	output, err := (*testCmder)(c.Cmd(provided, wd)).Run()
+	require.NoError(t, err)
+
+	expected := "Cmd: /bin/test, args: [prepended 1 prepended 2 provided 1 provided 2], cmdWd: wd"
+	assert.Equal(t, expected, string(output))
+}
