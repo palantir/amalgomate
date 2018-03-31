18b0d7149c65339e863131ce62465dfbc496de9c
Nick Miyake <nmiyake@users.noreply.github.com>
2018-03-31 16:57:36 -0700

    Move core logic to amalgomateplugin package (#30)

--- godelplugin/cmd/run.go
+++ godelplugin/cmd/run.go
@@ -9,19 +9,13 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
-	"sort"
-	"strings"
 
-	"github.com/palantir/godel/pkg/dirchecksum"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 
-	"github.com/palantir/amalgomate/cmd/amalgomate"
 	"github.com/palantir/amalgomate/godelplugin/amalgomateplugin"
 	"github.com/palantir/amalgomate/godelplugin/amalgomateplugin/config"
 )
@@ -45,76 +39,8 @@ var runCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		return runAmalgomate(param, verifyFlag, cmd.OutOrStdout())
+		return amalgomateplugin.Run(param, verifyFlag, cmd.OutOrStdout())
 	},
-}
-
-const indentLen = 2
-
-func runAmalgomate(param amalgomateplugin.Param, verify bool, stdout io.Writer) error {
-	var verifyFailedKeys []string
-	verifyFailedErrors := make(map[string]string)
-	verifyFailedFn := func(name, errStr string) {
-		verifyFailedKeys = append(verifyFailedKeys, name)
-		verifyFailedErrors[name] = errStr
-	}
-
-	sortedKeys := param.OrderedKeys
-	if len(sortedKeys) == 0 {
-		for k := range param.Amalgomators {
-			sortedKeys = append(sortedKeys, k)
-		}
-		sort.Strings(sortedKeys)
-	}
-	for _, k := range param.OrderedKeys {
-		val := param.Amalgomators[k]
-		cfg, err := amalgomate.LoadConfig(val.Config)
-		if err != nil {
-			return errors.Wrapf(err, "failed to read amalgomate configuration for %s", k)
-		}
-
-		if verify {
-			if _, err := os.Stat(val.OutputDir); os.IsNotExist(err) {
-				verifyFailedFn(k, fmt.Sprintf("output directory %s does not exist", val.OutputDir))
-				continue
-			}
-
-			originalChecksums, err := dirchecksum.ChecksumsForMatchingPaths(val.OutputDir, nil)
-			if err != nil {
-				return errors.Wrapf(err, "failed to compute original checksums")
-			}
-
-			newChecksums, err := dirchecksum.ChecksumsForDirAfterAction(val.OutputDir, func(dir string) error {
-				if err := amalgomate.Run(cfg, dir, val.Pkg); err != nil {
-					return err
-				}
-				return nil
-			})
-			if err != nil {
-				return errors.Wrapf(err, "amalgomate verify failed for %s", k)
-			}
-
-			if diff := originalChecksums.Diff(newChecksums); len(diff.Diffs) > 0 {
-				verifyFailedFn(k, diff.String())
-				continue
-			}
-			continue
-		}
-		if err := amalgomate.Run(cfg, val.OutputDir, val.Pkg); err != nil {
-			return errors.Wrapf(err, "amalgomate failed for %s", k)
-		}
-	}
-	if verify && len(verifyFailedKeys) > 0 {
-		fmt.Fprintf(stdout, "amalgomator output differs from what currently exists: %v\n", verifyFailedKeys)
-		for _, currKey := range verifyFailedKeys {
-			fmt.Fprintf(stdout, "%s%s:\n", strings.Repeat(" ", indentLen), currKey)
-			for _, currErrLine := range strings.Split(verifyFailedErrors[currKey], "\n") {
-				fmt.Fprintf(stdout, "%s%s\n", strings.Repeat(" ", indentLen*2), currErrLine)
-			}
-		}
-		return fmt.Errorf("")
-	}
-	return nil
 }
 
 func init() {
